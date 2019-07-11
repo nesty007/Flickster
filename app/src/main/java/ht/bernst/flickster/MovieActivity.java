74ccc00c11ce2a3b1d2f529f534b478d79fce330
Bernst G. Nestor <50707863+nesty007@users.noreply.github.com>
2019-07-11 04:06:43 -0400

    Import movie DB
    import movie DB

--- app/src/main/java/ht/bernst/flickster/MovieActivity.java
+++ app/src/main/java/ht/bernst/flickster/MovieActivity.java
@@ -3,12 +3,49 @@ package ht.bernst.flickster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
+import ht.bernst.flickster.models.Movie;
 
 public class MovieActivity extends AppCompatActivity {
+
+    private static final String MOVIE_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+
+    List<Movie> movies;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.movie_main);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(MOVIE_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                  JSONArray movieJsonArray = response.getJSONArray("results");
+                  movies = Movie.fromJsonArray(movieJsonArray);
+                  Log.d("smile", movies.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+        });
     }
 }
